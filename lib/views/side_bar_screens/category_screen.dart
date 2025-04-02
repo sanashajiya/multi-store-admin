98ea417dd8d4c4a6517b2ede3f3802987cea2dbf
sanashajiya <ssanashajiya@gmail.com>
2025-04-02 10:07:54 +0530

    Add File Picker for Category Image Selection

--- lib/views/side_bar_screens/category_screen.dart
+++ lib/views/side_bar_screens/category_screen.dart
@@ -1,13 +1,90 @@
-import 'package:flutter/cupertino.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
 
-class CategoryScreen extends StatelessWidget {
-  static const String id = '\category-screen';
+class CategoryScreen extends StatefulWidget {
+  static const String id = 'category-screen';
   const CategoryScreen({super.key});
 
   @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  dynamic _image;
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("CategoryScreen"),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Categories',
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Divider(color: Colors.grey),
+        ),
+
+        Row(
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: _image!=null ? Image.memory(_image) : Center(child: Text('Category image')),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 200,
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: 'Enter Category Name'),
+                ),
+              ),
+            ),
+
+            TextButton(onPressed: () {}, child: Text("Cancel")),
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+              onPressed: () {},
+              child: Text('Save', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(onPressed: () {
+            pickImage();
+          }, child: Text("Pick Image")),
+        ),
+
+        Divider(color: Colors.grey),
+      ],
     );
   }
 }

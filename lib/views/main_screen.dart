69bf75f9e195ec68a1782d6ec6b80e6cec91c27f
sanashajiya <ssanashajiya@gmail.com>
2025-04-02 08:28:53 +0530

    Add Sidebar Structure using AdminScaffold

---
+++ lib/views/main_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AdminScaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+       title: Text("Management"),
+      ),
+      body: Text(
+        'Dashboard'
+        ),
+      sideBar: SideBar(items: [
+        AdminMenuItem(title: "Vendors", route: "", icon: CupertinoIcons.person_3),
+        AdminMenuItem(title: "Buyer", route: "", icon: CupertinoIcons.person),
+        AdminMenuItem(title: "Categories", route: "", icon:Icons.category),
+        AdminMenuItem(title: "Banners", route: "", icon: CupertinoIcons.bandage),
+      ], selectedRoute: ''),
+    );
+  }
+}

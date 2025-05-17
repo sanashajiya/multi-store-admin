fefe94786b0c6fe15d451809b7df3b9312935b15
sanashajiya <ssanashajiya@gmail.com>
2025-05-17 08:47:12 +0530

    display vendors

--- lib/controllers/vendor_controller.dart
+++ lib/controllers/vendor_controller.dart
@@ -1,33 +1,33 @@
 import 'dart:convert';
 
+import 'package:web_app_admin_panel/models/vendor.dart';
 import 'package:http/http.dart' as http;
-import 'package:web_app_admin_panel/models/buyer.dart';
 
 import '../global_variable.dart';
 
-class BuyerController {
+class VendorController {
   // load buyers
-  Future<List<BuyerModel>> fetchBuyers() async {
+  Future<List<VendorModel>> fetchVendors() async {
     try {
       http.Response response = await http.get(
-        Uri.parse('$uri/api/users'),
+        Uri.parse('$uri/api/vendors'),
         headers: <String, String>{
           "Content-Type": 'application/json; charset=UTF-8',
         },
       );
 
-      // print(response.body);
+      print(response.body);
 
       if (response.statusCode == 200) {
         List<dynamic> data = jsonDecode(response.body);
-        List<BuyerModel> buyers =
-            data.map((buyer) => BuyerModel.fromMap(buyer)).toList();
-        return buyers;
+        List<VendorModel> vendors =
+            data.map((vendor) => VendorModel.fromMap(vendor)).toList();
+        return vendors;
       } else {
-        throw Exception('Failed to load buyers');
+        throw Exception('Failed to load Vendors');
       }
     } catch (e) {
-      throw Exception('Error loading buyers:$e');
+      throw Exception('Error loading Vendors:$e');
     }
   }
 }
